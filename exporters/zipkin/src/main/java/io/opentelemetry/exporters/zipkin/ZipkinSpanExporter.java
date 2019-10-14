8c6f4d7c42773ef171f35a056a98fca4a4874de8
Brian Devins-Suresh <badevins@gmail.com>
2019-10-14 10:59:16 -0400

    not sure what to do with the failed sends

--- exporters/zipkin/src/main/java/io/opentelemetry/exporters/zipkin/ZipkinSpanExporter.java
+++ exporters/zipkin/src/main/java/io/opentelemetry/exporters/zipkin/ZipkinSpanExporter.java
@@ -4,24 +4,35 @@ import io.opentelemetry.sdk.trace.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import zipkin2.Endpoint;
 import zipkin2.Span;
+import zipkin2.codec.SpanBytesEncoder;
+import zipkin2.reporter.Sender;
 
 import javax.annotation.concurrent.ThreadSafe;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 @ThreadSafe
 public final class ZipkinSpanExporter implements SpanExporter {
+  private final Sender sender;
+  private final SpanBytesEncoder encoder;
+
   private final Endpoint localEndpoint;
 
-  private ZipkinSpanExporter() {
-    Endpoint.Builder endpointBuilder = Endpoint.newBuilder();
-    endpointBuilder.serviceName("unknown");
-    endpointBuilder.port(0);
+  private ZipkinSpanExporter(String serviceName, Integer servicePort, Sender sender, SpanBytesEncoder encoder) {
+    this.sender = sender;
+    this.encoder = encoder;
+
+    Endpoint.Builder endpointBuilder = Endpoint.newBuilder()
+            .serviceName(serviceName);
+    if (servicePort != null) {
+      endpointBuilder.port(servicePort);
+    }
 
     try {
-      String ip = InetAddress.getLocalHost().getHostAddress();
-      endpointBuilder.ip(ip);
+      endpointBuilder.ip(InetAddress.getLocalHost().getHostAddress());
     } catch (UnknownHostException ignored) {
     }
 
@@ -31,11 +42,35 @@ public final class ZipkinSpanExporter implements SpanExporter {
   @Override
   public ResultCode export(List<SpanData> spanDataList) {
     List<Span> spans = SpanAdapter.toZipkin(localEndpoint, spanDataList);
-    return null;
+
+    try {
+      List<byte[]> batch = new ArrayList<>();
+      int currentSize = 0;
+      for (Span span : spans) {
+        byte[] single = encoder.encode(span);
+        int singleSize = sender.messageSizeInBytes(single.length);
+        if (currentSize + singleSize < sender.messageMaxBytes()) {
+          batch.add(single);
+          currentSize += singleSize;
+        } else {
+          sender.sendSpans(batch).execute();
+          batch.clear();
+          batch.add(single);
+          currentSize = singleSize;
+        }
+      }
+      if (!batch.isEmpty()) {
+        sender.sendSpans(batch).execute();
+      }
+    } catch (IOException e) {
+      return ResultCode.FAILED_RETRYABLE;
+    }
+
+    return ResultCode.SUCCESS;
   }
 
   @Override
   public void shutdown() {
-
+    // Nothing to shutdown
   }
 }
